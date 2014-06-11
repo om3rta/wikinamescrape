3adad6ebc1816b8d84594d03e7ad8fe45c2f5151
om3rta <spamitycalamity@gmail.com>
2014-06-11 19:20:08 -0400

    all functions built into class

--- wikinamescrape.py/wikinamescrape.py
+++ wikinamescrape.py/wikinamescrape.py
@@ -1,50 +1,53 @@
 #WikiNameScrape gathers names for use in testing from Wikipedia
-
 import urllib2
-import re
 from BeautifulSoup import BeautifulSoup
 
 
 class WikiNameScrape:
 
     def grab_name(self):
-        soup = BeautifulSoup(urllib2.urlopen('http://en.wikipedia.org/wiki/Special:Random').read())
-        wikititle = soup.find('title')
-        wikistr = str(wikititle)
-        wikisplit = wikistr.split(" ", 1)
-        wikilongname = wikisplit[1]
-        wikisplitlong = wikilongname.split(" ", 1)
-        finalname = wikisplitlong[0]
-        return finalname
+        count = 0
+        while count != 10:
+            soup = BeautifulSoup(urllib2.urlopen('http://en.wikipedia.org/wiki/Special:Random').read())
+            wikititle = soup.find('title')
+            wikistr = str(wikititle)
+            wikisplit = wikistr.split(" ", 1)
+            wikilongname = wikisplit[1]
+            wikisplitlong = wikilongname.split(" ", 1)
+            finalname = wikisplitlong[0]
+            return finalname
+
+    def dict_add(self, dict_name):
+            count = 0
+            while count != 10:
+                name = WikiNameScrape().grab_name()
+                if str.isalpha(name):
+                    dict_name['User_' + str(count)]['first'] = name
+                    count = count + 1
+                    print count
+            count = 0
+            while count != 10:
+                name = WikiNameScrape().grab_name()
+                if str.isalpha(name):
+                    dict_name['User_' + str(count)]['last'] = name
+                    count = count + 1
+                    print count
 
 
+names = {
+    'User_0': {'first': None, 'last': None},
+    'User_1': {'first': None, 'last': None},
+    'User_2': {'first': None, 'last': None},
+    'User_3': {'first': None, 'last': None},
+    'User_4': {'first': None, 'last': None},
+    'User_5': {'first': None, 'last': None},
+    'User_6': {'first': None, 'last': None},
+    'User_7': {'first': None, 'last': None},
+    'User_8': {'first': None, 'last': None},
+    'User_9': {'first': None, 'last': None}
+}
 
-name_class = WikiNameScrape()
-name = name_class.grab_name()
+scraper = WikiNameScrape()
+scraped_name = scraper.dict_add(names)
 
-names = {'first': {},'last': {}}
-
-count = 0
-while count != 10:
-    name_class = WikiNameScrape()
-    name = name_class.grab_name()
-    if str.isalpha(name):
-        names['first']['first' + str(count)] = name
-        name_class = WikiNameScrape()
-        name = name_class.grab_name()
-        if str.isalpha(name):
-            names['last']['last' + str(count)] = name
-            count = count + 1
-            print count
-
-
-
-print "example name: " + names['first']['first1'] + " " + names['last']['last1']
-
-
-#finalname will end up being the first word in the title of a wiki article. This will sometimes end up being a character
-#that is not alphanumeric. Need to run a check to make sure the string only contains letters and numbers
-
-#need to add finalname to a dictionary. Need to end up with 20 first names and 20 last names
-
-#dicktname.update({x:x})
+print "example name: " + names['User_0']['first'] + " " + names['User_0']['last']
